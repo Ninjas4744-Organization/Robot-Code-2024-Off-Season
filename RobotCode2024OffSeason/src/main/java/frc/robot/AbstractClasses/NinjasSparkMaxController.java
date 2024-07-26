4674e31dfb12c5ca8dd9b9dcc89f52132d138b57
eitanVIP <eitangrimblat@gmail.com>
2024-07-26 21:35:23 +0300

    NinjasSparkMaxController

--- RobotCode2024OffSeason/src/main/java/frc/robot/AbstractClasses/NinjasSparkMaxController.java
+++ RobotCode2024OffSeason/src/main/java/frc/robot/AbstractClasses/NinjasSparkMaxController.java
@@ -3,14 +3,13 @@ package frc.robot.AbstractClasses;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import frc.robot.DataClasses.ControllerConstants;
 
 public class NinjasSparkMaxController extends NinjasController
 {
     private CANSparkMax _controller;
-    private TrapezoidProfile _profile;
-    private SparkPIDController _pidController;
 
     public NinjasSparkMaxController(ControllerConstants constants)
     {
@@ -19,12 +18,6 @@ public class NinjasSparkMaxController extends NinjasController
         _controller = new CANSparkMax(constants.id, CANSparkMax.MotorType.kBrushless);
         _controller.restoreFactoryDefaults();
         _controller.setInverted(constants.invertOutput);
-        _pidController = _controller.getPIDController();
-        _pidController.setP(constants.pidConstants.kP);
-        _pidController.setI(constants.pidConstants.kI);
-        _pidController.setD(constants.pidConstants.kD);
-        _pidController.setIZone(constants.pidConstants.iZone);
-        _profile = new TrapezoidProfile(constants.constraints);
         _controller.burnFlash();
     }
 
@@ -33,40 +26,65 @@ public class NinjasSparkMaxController extends NinjasController
         switch (_controlState) {
             case PERCENT_OUTPUT:
                 return _controller.get();
-        
-            case MOTION_MAGIC:
+
+            case POSITION_PIDF:
+                return _controller.getEncoder().getPosition();
+
+            case VELOCITY_PIDF:
+                return _controller.getEncoder().getVelocity();
+
+            default:
                 return -1;
         }
     }
 
     @Override
     public void set(double value) {
+        switch (_controlState) {
+            case PERCENT_OUTPUT:
+                _controller.set(value);
+                break;
+            
+            case MOTION_MAGIC:
+                break;
 
+            case POSITION_PIDF:
+                _pidfController.setGoal(new TrapezoidProfile.State(value, 0));
+                break;
+
+            case VELOCITY_PIDF:
+                _pidfController.setGoal(new TrapezoidProfile.State(0, value));
+                break;
+        }
     }
 
     @Override
     public void stop() {
-        
+        _controller.stopMotor();
     }
 
     @Override
-    public double getEncoder() {
-        
+    public TrapezoidProfile.State getEncoder() {
+        return new TrapezoidProfile.State(_controller.getEncoder().getPosition(), _controller.getEncoder().getVelocity());
+    }
+
+    @Override
+    public double getOutput(){
+        return _controller.get();
     }
 
     @Override
     public void setEncoder(double position) {
-        
-    }
-
-    @Override
-    public double getSetpoint() {
-        
-    }
-
-    @Override
-    protected void updateShuffleboard() {
-        
+        _controller.getEncoder().setPosition(position);
     }
     
+    @Override
+    public void periodic() {
+        super.periodic();
+        
+        if(_controlState == ControlState.POSITION_PIDF)
+            _controller.set(_pidfController.calculate(getEncoder().position, getSetpointState()));
+        else if(_controlState == ControlState.VELOCITY_PIDF)
+            _controller.set(_pidfController.calculate(getEncoder().velocity, getSetpointState()));
+    }
 }
