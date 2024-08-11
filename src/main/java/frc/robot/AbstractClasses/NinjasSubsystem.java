9af168eaf4305d09cb3d9a8e15563eb2f4529d4d
GGreenix <76048260+GGreenix@users.noreply.github.com>
2024-08-11 21:07:01 +0300

    Update NinjasSubsystem.java

--- src/main/java/frc/robot/AbstractClasses/NinjasSubsystem.java
+++ src/main/java/frc/robot/AbstractClasses/NinjasSubsystem.java
@@ -1,5 +1,62 @@
 package frc.robot.AbstractClasses;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public abstract class NinjasSubsystem extends SubsystemBase {}
+public abstract class NinjasSubsystem extends SubsystemBase {
+    protected NinjasController _group;
+    RobotStates _currentState;
+
+    
+    public NinjasSubsystem(NinjasController groupOfMotors){
+        _group = groupOfMotors;
+    }
+
+    public Command runMotor(double percetage){
+        return Commands.startEnd(
+                ()->{
+                    _group.setPercent(percetage);
+                },
+                _group::stop,
+              this);
+    }
+
+    public enum RobotStates{
+        INTAKE,
+        POOPING,
+        SHOOTING,
+        CLIMBING,
+        IDLE,
+        STOP,
+        MANUAL,
+        RESET,
+      }
+
+    @Override
+    public void periodic() {
+        switch (_currentState) {
+            case INTAKE:
+              intake();
+              break;
+            case STOP:
+              _group.stop();
+              break;
+            case MANUAL:
+              manual();
+              break;
+            case RESET:
+              reset();
+              break;
+            default:
+              idle();
+              break;
+          }
+        _group.periodic();
+    }
+
+    public abstract void intake();
+    public abstract void idle();
+    public abstract void manual();
+    public abstract void reset();
+}
